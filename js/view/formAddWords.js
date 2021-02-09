f6e984b2d97195a48b9d81aaa8771855ea457e6e
ana.paula.da.s.rocha <apdsrocha@gmail.com>
2021-02-09 16:19:43 -0300

    adds view to manipulate form to include words

---
+++ js/view/formAddWords.js
@@ -0,0 +1,22 @@
+import * as WordController from '../controller/WordController.js';
+import { showCards } from './divWordsList.js';
+
+const btnSave = document.querySelector('#btn-save');
+const inputFR = document.querySelector('#input-wordFR');
+const inputBR = document.querySelector('#input-wordBR');
+
+btnSave.addEventListener('click', async function() {
+  try 
+  {
+    let formAnswers = {
+      wordFR : inputFR.value,
+      wordPT : inputBR.value
+    }    
+      await WordController.sendWord(formAnswers);
+      await showCards();
+  }
+  catch(erro) 
+  { 
+    alert('Um erro inesperado ocorreu ao adicionar um produto ao seu pedido. Por favor, contate o administrador!');
+  }
+});
