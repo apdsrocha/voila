d73dbc443964b147d7636f0c199f4e420b0d6418
ana.paula.da.s.rocha <apdsrocha@gmail.com>
2021-02-09 16:23:46 -0300

    adds service to include words

--- js/services/WordsService.js
+++ js/services/WordsService.js
@@ -3,8 +3,21 @@ const URL_BASE = 'http://localhost:5000/api/';
 export async function getWords()
 {
     const resposta = await fetch(URL_BASE);
-
     const wordList = await resposta.json();
-    console.log(wordList.data)
     return wordList.data;
+}
+
+export async function saveWord(newWord)
+{
+    const answer = await fetch(URL_BASE, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newWord)
+      });
+    const statusServer= await answer.json();
+    
+    return statusServer;
 }
