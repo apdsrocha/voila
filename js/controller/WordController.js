c5babf9b3e36103e2e98081ac828131df2165f73
ana.paula.da.s.rocha <apdsrocha@gmail.com>
2021-02-10 16:48:23 -0300

    new logic for adding non-repeating ids

--- js/controller/WordController.js
+++ js/controller/WordController.js
@@ -1,5 +1,5 @@
 import Word from "../model/Word.js";
-import { getWords, saveWord } from "../services/WordsService.js";
+import { getWords, saveWord, deleteWord } from "../services/WordsService.js";
 
 export async function getListofWords()
 {
@@ -12,8 +12,24 @@ export async function sendWord(formProduto)
     const word = new Word();
     word.wordPT = formProduto.wordPT;
     word.wordFR = formProduto.wordFR;
-    let id = await getListofWords();
-    word.id =  id.length;
+
+    let allWords = await getListofWords();
+
+    if(!!allWords.length) {
+        let ids = allWords.map(item => item.id);
+        let sortedIds = ids.sort((a, b) => a - b);
+        let greatestNumber = sortedIds[sortedIds.length -1];
+        word.id =  greatestNumber + 1;
+    } else {
+        word.id =  id.length;
+    }
+
    const status = await saveWord(word);
    return status;
+}
+
+export async function eraseCard(id)
+{
+    const wordsInServer = await deleteWord(id);
+    return wordsInServer
 }
